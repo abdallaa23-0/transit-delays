667b88a2c0eb92c22ca30c3e2c09dc922396c117
Abdalla_Ali <abdalla.ali1136@gmail.com>
2026-03-10 17:26:37 -0600

    done

--- src/train_model.py
+++ src/train_model.py
@@ -9,11 +9,12 @@ data_apath = "data/processed/training_data.csv"
 df = pd.read_csv(data_apath)
 features = ['stop_lat', 'stop_lon','arrival_min','stop_sequence']
 X = df[features].dropna()
-y = df.loc[X.index, 'scheduled_arrival_time']
+# Change this line in train_model.py:
+y = df.loc[X.index, 'scheduled_travel_time']  # Was 'scheduled_arrival_time'
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
 print("Training delay model")
-model = RandomForestRegressor(n_estimators=100,random_state=42)
+model = RandomForestRegressor(n_estimators=20, max_depth=10,random_state=42)
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
